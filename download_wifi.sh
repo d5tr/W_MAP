3ab3cf1441bdf8fa5558d440fd8a35b0007a4b40
D5TR <72051237+d5tr@users.noreply.github.com>
2021-04-20 05:44:00 +0300

    Add files via upload

---
+++ download_wifi.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+pip3 install time
+
+pip3 install socket
+
+python3 -m pip install scapy
+
+pip3 install subprocess
+
+pip3 install colorama
+
+pip3 install inquirer
+
+python3 -m pip install scapy
+
+echo "Done ..."
+
+
