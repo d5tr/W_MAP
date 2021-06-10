565e3215b1ab3c07984ed4c248a497ef27c8b373
D5TR <72051237+d5tr@users.noreply.github.com>
2021-06-10 11:00:01 +0300

    Add files via upload

---
+++ ban_handshake.py
@@ -0,0 +1,30 @@
+from colorama import Fore
+def BH():
+
+    print(Fore.GREEN+'''
+'                       :::!~!!!!!:.
+'                  .xUHWH!! !!?M88WHX:.
+'                .X*#M@$!!  !X!M$$$$$$WWx:.
+'               :!!!!!!?H! :!$!$$$$$$$$$$8X:
+'              !!~  ~:~!! :~!$!#$$$$$$$$$$8X:
+'             :!~::!H!<   ~.U$X!?R$$$$$$$$MM!     MAKE BY ---> @d_5tr
+'             ~!~!!!!~~ .:XW$$$U!!?$$$$$$RMM!     TEAM ---> @ZER0ONE_01 
+'               !:~~~ .:!M"T#$$$$WX??#MRRMMM!  
+'               ~?WuxiW*`   `"#$$$$8!!!!??!!!
+'             :X- M$$$$       `"T#$T~!8$WUXU~          Don't be script kidde and learn hacking ...
+'            :%`  ~#$$$m:        ~!~ ?$$$$$$
+'          :!`.-   ~T$$$$8xx.  .xWW- ~""##*"
+'.....   -~~:<` !    ~?T#$$@@W@*?$$      /`
+'W$@@M!!! .!~~ !!     .:XUW$W!~ `"~:    :
+'#"~~`.:x%`!!  !H:   !WM$$$$Ti.: .!WUn+!`
+':::~:!!`:X~ .: ?H.!u "$$$B$$$!W:U!T$$M~
+'.~~   :X@!.-~   ?@WTWo("*$$$W$TH$! `
+'Wi.~!X$?!-~    : ?$$$B$Wu("**$RM!
+'$R@i.~~ !     :   ~$$$$$B$$en:``
+'?MXT@Wx.~    :     ~"##*$$$$M~
+'    
+                     [1] Handshake All Encryptions
+                     [2] Handshake WEP Only
+                     [3] Handshake WPA Only 
+                     [99] Back
+    ''')
---
+++ download_W-MAP.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+
+chmod +x password_wifi.sh
+
+chmod +x killerr.sh
+
+apt-get install xterm
+
+pip3 install time
+
+pip3 install socket
+
+python3 -m pip install scapy
+
+pip3 install subprocess
+
+pip3 install colorama
+
+pip3 install inquirer
+
+python3 -m pip install scapy
+
+echo "Done download ..."
