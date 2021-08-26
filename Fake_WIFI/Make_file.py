2daa6e1e478c297d64fdf35b8808b70afcc070ed
D5TR <72051237+d5tr@users.noreply.github.com>
2021-08-26 17:09:01 +0300

    Delete Fake_WIFI directory

--- Fake_WIFI/Make_file.py
+++
@@ -1,8 +0,0 @@
-from colorama import Fore
-
-def Make_file_for_wifi():
-    while True:
-        print(f'{Fore.RED}Type one name at a time{Fore.WHITE}')
-        print(f'{Fore.RED}To stop writing network names{Fore.WHITE}')
-        Names = input(f'\n[{Fore.CYAN}?{Fore.WHITE}]Enter name WI-FI : ')
-
